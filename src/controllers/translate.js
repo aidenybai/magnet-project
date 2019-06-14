af5af96bae78b5a6d717436b8f6e614237892b76
Aiden Bai <mail.contraband@gmail.com>
2019-06-14 08:36:27 -0700

    Update router from GET to POST

--- src/controllers/translate.js
+++ src/controllers/translate.js
@@ -3,16 +3,16 @@ import translate from '@vitalets/google-translate-api';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  if (!req.query.text) {
+router.post('/', (req, res) => {
+  if (!req.body.text) {
     res.status(500).json({ code: 500, message: 'No text specified' });
   }
-  else if (!req.query.to) {
+  else if (!req.body.to) {
     res.status(500).json({ code: 500, message: 'No target language specified' });
   }
-  else if (!req.query.from) {
-    translate(decodeURIComponent(req.query.text), { to: req.query.to }).then(output => {
-      res.status(200).json({ code: 200, message: output.text, lang: { from: output.from.language.iso, to: req.query.to, autocorrect: output.from.text.autoCorrected } });
+  else if (!req.body.from) {
+    translate(decodeURIComponent(req.body.text), { to: req.body.to }).then(output => {
+      res.status(200).json({ code: 200, message: output.text, lang: { from: output.from.language.iso, to: req.body.to, autocorrect: output.from.text.autoCorrected } });
     }).catch(err => {
       res.status(500).json({ code: 500, message: 'Error when translating' });
     });
